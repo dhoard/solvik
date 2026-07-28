656f730f244459e4369728b3b775f0cf5233509e
Doug Hoard <dhoard@users.noreply.github.com>
2026-07-27 23:11:24 -0400

    fix: Brace usage requirements (#10)
    Signed-off-by: dhoard <doug.hoard@gmail.com>

--- test/switch_test.sol
+++ test/switch_test.sol
@@ -2,11 +2,14 @@ package example
 func main() -> int {
     x: int = 42
     switch x {
-        case 10:
+        case 10: {
             return 1
-        case 42:
+        }
+        case 42: {
             return 0
-        default:
+        }
+        default: {
             return 2
+        }
     }
 }
